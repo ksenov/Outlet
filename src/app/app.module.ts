862830c4e39a1392348c5d7e892239fd99f04625
ksenov <xenofontb@gmail.com>
2023-09-04 12:48:06 +0900

    add cart srevice and change filters

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,6 +25,8 @@ import { ProductBoxComponent } from './pages/home/components/product-box/product
 import { CartComponent } from './pages/cart/cart.component';
 
 import { CartService } from './services/cart.service';
+import { StoreService } from './services/store.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -51,9 +53,10 @@ import { CartService } from './services/cart.service';
     MatToolbarModule,
     MatTableModule,
     MatBadgeModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    HttpClientModule
   ],
-  providers: [CartService],
+  providers: [CartService, StoreService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/pages/home/components/product-box/product-box.component.ts
+++ src/app/pages/home/components/product-box/product-box.component.ts
@@ -7,14 +7,7 @@ import { Product } from 'src/app/models/product.model';
 })
 export class ProductBoxComponent implements OnInit {
   @Input() fullWidthMode = false;
-  product: Product | undefined = {
-    id: 1,
-    title: 'Кроссовки',
-    price: 150,
-    category: 'Обувь',
-    description: 'Описание',
-    image: 'https://via.placeholder.com/150'
-  };
+  @Input() product: Product | undefined;
 
   @Output() addToCart = new EventEmitter()
 
