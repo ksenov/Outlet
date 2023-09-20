8a079ce54817ff103f9b41b72f427228f61170fd
ksenov <xenofontb@gmail.com>
2023-09-20 13:36:30 +0900

    connect to firebase

---
+++ src/environments/environment.default.ts
@@ -0,0 +1,12 @@
+export const defaultEnvironment = {
+    firebase: {
+      apiKey: "AIzaSyAxoIVmZpFgm6vbzQS-N8DYzrGciX3r60Y",
+      authDomain: "outlet-auth.firebaseapp.com",
+      projectId: "outlet-auth",
+      storageBucket: "outlet-auth.appspot.com",
+      messagingSenderId: "366126030282",
+      appId: "1:366126030282:web:b4cfc8b3544c4662568333",
+      measurementId: "G-VM6ETK21D3"
+    },
+    production: false
+  };
